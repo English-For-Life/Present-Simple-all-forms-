75c15443ef78124bb624ad47b5f5787024bd66f8
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:21:52 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"___ your father go to work every day?",
+        options:["Do", "Does", "Is", "Has"],
+        answer: 1
+    },
+    {
+     q:"___ your friends like playing football?",
+        options:["Do", "Are", "Does", "Have"],
+        answer: 0   
+    },
+    {
+        q:"___ your dog like meat?",
+        options:["Are", "Do", "Does", "Have"],
+        answer: 2
+    },
+    {
+        q:"Alex and Ann always ___ breakfast in the morning.",
+        options:["had", "has", "haves", "have"],
+        answer: 3
+    },
+    {
+        q:"My mum ___ cooking.",
+        options:["liks", "like", "likes", "liking"],
+        answer: 2
+    },
+    {
+        q:"My dad always ___ his car.",
+        options:["washing", "washes", "washs", "wash"],
+        answer: 1
+    },
+    {
+        q:"I ___ reading.",
+        options:["don't like", "don't likes", "doesn't like", "am not"],
+        answer: 0
+    },
+    {
+        q:"He ___ play guitar every day.",
+        options:["don't plays", "don't play", "doesn't plays", "doesn't play"],
+        answer: 3
+    },
+    {
+        q:"They ___ to speak with you.",
+        options:["doesn't want", "don't want", "don't wants", "aren't want"],
+        answer: 1
+    },
+    {
+        q:"I ___ to music every day.",
+        options:["listened", "listens", "listen", "listening"],
+        answer: 2
+    }
+
+]
